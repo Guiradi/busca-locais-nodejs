01b4d825e504dc01d51a60ffb373f805265f10a2
Guiradi <g.ferreira@unesp.br>
2020-07-04 17:11:45 -0300

    Remove path não utilizado

--- config.js
+++ config.js
@@ -1,7 +1,6 @@
 module.exports = {
   GOOGLE_PLACES_API_KEY: "AIzaSyBzpjoYQ-oZcudwZnwd7aGprmfn7e8KsY0",
   GOOGLE_PLACES_OUTPUT_FORMAT: "json",
-  FILE_PATH: "places.csv",
   location: '-21.7842308,-48.1793184',
   radius: '5000' // metros
 };
