9cf9efe528e8317735d4ceb0c5e8f82ad55f3eba
Guiradi <g.ferreira@unesp.br>
2020-07-08 23:33:43 -0300

    Comenta os métodos

--- index.js
+++ index.js
@@ -4,6 +4,12 @@ const GooglePlaces = require('./GooglePlaces')
 const TypeList = require('./utils/TypeEnum')
 
 const G = new GooglePlaces()
+
+// Escolhe 1 método específico
 // G.fetchType(Object.keys(TypeList)[0])
+
+// Busca todos os métodos em uma única planilha
 // G.fetchAllPlacesType()
+
+// Busca todos os métodos em planilhas diferentes, simultâneamente
 // Object.keys(TypeList).forEach((type) => G.fetchType(type))
